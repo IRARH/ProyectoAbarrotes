854b12d47cfc9cf0e11c815533fdaa8aee4c8a72
Miguel Angel Castañeda <miguel.macm.ca@gmail.com>
2020-06-04 18:21:40 -0500

    Rama: master Modificaciones: Combinacion del Menu y Notas

---
+++ Notas_menu.php
@@ -0,0 +1,172 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Crear Nota</title>
+    <meta charset="utf-8">
+
+    <link rel="stylesheet" href="styles_menu_notas.css" />
+</head>
+
+
+<body>
+    <div class="navegacion">
+      <!-- <img src="extrella_menu.jpg" title="logo" alt="logo_tienda" width="200" height="70" />-->
+      <a href="#"></a>  
+      <a href="#">Registrar Nuevo</a>
+        <a href="#">Actualizar Stock</a>
+        <a href="#">Eliminar Producto</a>
+        <a href="#">Ver y Buscar</a>
+        <a href="#">Registrar Nota</a>
+        <a href="#">Estadísticas</a>
+    </div>
+
+    <nav id="principal">
+        <h2>Tienda de abarrotes estrella</h2>
+    </nav>
+
+    <div id="palabras">
+        <section id="principal_notas">
+
+            <header id="encabezado">
+                <h2>Registro de Nota </h2>
+            </header>
+
+            <div id="formulario">
+
+                <form action="#" method="POST">
+                    <label for="codigo_barra">Ingresa Codigo de Barra</label>
+                    <input type="text" name="correo" id="correo" required placeholder="" />
+                    <label for="destinatario">Destinatario</label>
+                    <input type="text" name="correo" id="correo" required placeholder="" />
+
+                    <div id="div1">
+                        <table>
+                            <tr>
+                                <td class=color>Codigo de Barras</td>
+                                <td class=color> Nombre de Producto</td>
+                                <td class=color>Marca</td>
+                                <td class=color>Tipo</td>
+                                <td class=color>Cabtidad</td class=color>
+                                <td class=color>Precio</td>
+                                <td class=color>Subtotal</td>
+                            </tr>
+                            <tr>
+                                <td>10201021</td>
+                                <td>Cerveza</td>
+                                <td>Corona</td>
+                                <td>Caguama</td>
+                                <td>10</td>
+                                <td>10</td>
+                                <td>500</td>
+                            </tr>
+                            <tr>
+                                <td>10201021</td>
+                                <td>Cerveza</td>
+                                <td>Victoria</td>
+                                <td>Caguama</td>
+                                <td>10</td>
+                                <td>10</td>
+                                <td>500</td>
+                            </tr>
+                            <tr>
+                                <td>10201021</td>
+                                <td>Cerveza</td>
+                                <td>Tecate</td>
+                                <td>Caguama</td>
+                                <td>10</td>
+                                <td>10</td>
+                                <td>500</td>
+                            </tr>
+                            <tr>
+                                <td>10201021</td>
+                                <td>Frijol</td>
+                                <td>Costeña</td>
+                                <td>Lata</td>
+                                <td>2</td>
+                                <td>$15</td>
+                                <td>30</td>
+                            </tr>
+
+                            <tr>
+                                <td>10201021</td>
+                                <td>Frijol</td>
+                                <td>Costeña</td>
+                                <td>Lata</td>
+                                <td>2</td>
+                                <td>$15</td>
+                                <td>30</td>
+                            </tr>
+
+                            <tr>
+                                <td>10201021</td>
+                                <td>Frijol</td>
+                                <td>Costeña</td>
+                                <td>Lata</td>
+                                <td>2</td>
+                                <td>$15</td>
+                                <td>30</td>
+                            </tr>
+
+                            <tr>
+                                <td>10201021</td>
+                                <td>Frijol</td>
+                                <td>Costeña</td>
+                                <td>Lata</td>
+                                <td>2</td>
+                                <td>$15</td>
+                                <td>30</td>
+                            </tr>
+                            <tr>
+                                <td>10201021</td>
+                                <td>Frijol</td>
+                                <td>Costeña</td>
+                                <td>Lata</td>
+                                <td>2</td>
+                                <td>$15</td>
+                                <td>30</td>
+                            </tr>
+                            <tr>
+                                <td>10201021</td>
+                                <td>Frijol</td>
+                                <td>Costeña</td>
+                                <td>Lata</td>
+                                <td>2</td>
+                                <td>$15</td>
+                                <td>30</td>
+                            </tr>
+                            <tr>
+                                <td>10201021</td>
+                                <td>Frijol</td>
+                                <td>Costeña</td>
+                                <td>Lata</td>
+                                <td>2</td>
+                                <td>$15</td>
+                                <td>30</td>
+                            </tr>
+                          
+
+
+                        </table>
+                    </div>
+
+                    <label class="label1">Total</label>
+
+                    <input type="text1" name="correo" id="correo" value="$2500" required placeholder="" />
+                </form>
+
+            </div>
+            <div id="final">
+                <input type="submit" value="Crear nota" />
+
+            </div>
+        </section>
+
+
+
+
+
+
+</body>
+
+</html>
