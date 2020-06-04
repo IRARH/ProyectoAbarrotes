0506de6bd8bb6cd3d746ab6f618ccbf25d6346be
Irvin Armando <irvin.reyes95@gmail.com>
2020-06-04 16:16:29 -0500

    Rama:master archivos menu y styles_menu

---
+++ menu.php
@@ -0,0 +1,50 @@
+<!Doctype html>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Menú Principal</title>
+    <link rel="stylesheet" href="styles_menu.css"/>
+</head>
+<body>
+
+
+<div class="navegacion">
+    <img  src="extrella_menu.jpg" title="logo" alt="logo_tienda" width="200" height="70"/>
+    <a href="#">Registrar Nuevo</a>
+    <a href="#">Actualizar Stock</a>
+    <a href="#">Eliminar Producto</a>
+    <a href="#">Ver y Buscar</a>
+    <a href="#">Registrar Nota</a>
+    <a href="#">Estadísticas</a>
+</div>
+
+<nav id="principal">
+  <h2>Tienda de abarrotes estrella</h2>
+</nav>
+<div id="palabras">
+   <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ad architecto adipisci sed corrupti ipsam maxime accusantium 
+        est blanditiis at pariatur,laudantium eos explicabo provident laboriosam totam excepturi repellat esse expedita!
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ad architecto adipisci sed corrupti ipsam maxime accusantium 
+        est blanditiis at pariatur,laudantium eos explicabo provident laboriosam totam excepturi repellat esse expedita!
+   </p>
+   <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ad architecto adipisci sed corrupti ipsam maxime accusantium 
+        est blanditiis at pariatur,laudantium eos explicabo provident laboriosam totam excepturi repellat esse expedita!
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ad architecto adipisci sed corrupti ipsam maxime accusantium 
+        est blanditiis at pariatur,laudantium eos explicabo provident laboriosam totam excepturi repellat esse expedita!
+   </p>
+   <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ad architecto adipisci sed corrupti ipsam maxime accusantium 
+        est blanditiis at pariatur,laudantium eos explicabo provident laboriosam totam excepturi repellat esse expedita!
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ad architecto adipisci sed corrupti ipsam maxime accusantium 
+        est blanditiis at pariatur,laudantium eos explicabo provident laboriosam totam excepturi repellat esse expedita!
+   </p>
+   <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ad architecto adipisci sed corrupti ipsam maxime accusantium 
+        est blanditiis at pariatur,laudantium eos explicabo provident laboriosam totam excepturi repellat esse expedita!
+   </p>
+</div>
+</body>
+</html> 
